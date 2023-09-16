f1aa9d4538336d2c393de4a038d378c832640e83
christoph88 <christoph88@users.noreply.github.com>
2023-09-16 09:23:13 +0200

    modify files so they can be used with a service account

--- scripts/downloadContainer.js
+++ scripts/downloadContainer.js
@@ -1,9 +1,13 @@
 import { google } from "googleapis";
+import { readFileSync } from "fs";
+import { writeFile } from "fs/promises";
 
 const tagmanager = google.tagmanager("v2");
 
 async function downloadContainer() {
+  const credentials = JSON.parse(readFileSync("./gcp-sa-key.json"));
   const auth = new google.auth.GoogleAuth({
+    credentials,
     scopes: ["https://www.googleapis.com/auth/tagmanager.readonly"],
   });
 
@@ -28,9 +32,9 @@ async function downloadContainer() {
       parent,
     });
 
-  await fs.writeFile("tags.json", JSON.stringify(tags.data, null, 2));
-  await fs.writeFile("triggers.json", JSON.stringify(triggers.data, null, 2));
-  await fs.writeFile("variables.json", JSON.stringify(variables.data, null, 2));
+  await writeFile("tags.json", JSON.stringify(tags.data, null, 2));
+  await writeFile("triggers.json", JSON.stringify(triggers.data, null, 2));
+  await writeFile("variables.json", JSON.stringify(variables.data, null, 2));
 }
 
 downloadContainer().catch(console.error);
