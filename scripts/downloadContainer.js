30cf41b59aedc7f22da03ed155a3d21b4dcee0dd
christoph88 <christoph88@users.noreply.github.com>
2023-09-16 09:19:19 +0200

    modify the script so it uses googleapis

--- scripts/downloadContainer.js
+++ scripts/downloadContainer.js
@@ -1,21 +1,36 @@
-import { TagManager } from "@google-cloud/tagmanager";
-const fs = require("fs").promises;
+import { google } from "googleapis";
+
+const tagmanager = google.tagmanager("v2");
 
 async function downloadContainer() {
-  const tagManager = new TagManager();
+  const auth = new google.auth.GoogleAuth({
+    scopes: ["https://www.googleapis.com/auth/tagmanager.readonly"],
+  });
+
+  const authClient = await auth.getClient();
+
   const parent = `accounts/${process.env.ACCOUNT_ID}/containers/${process.env.CONTAINER_ID}/workspaces/${process.env.WORKSPACE_ID}`;
 
-  const [tags] = await tagManager.accounts.containers.workspaces.tags.list({
+  const tags = await tagmanager.accounts.containers.workspaces.tags.list({
+    auth: authClient,
     parent,
   });
-  const [triggers] =
-    await tagManager.accounts.containers.workspaces.triggers.list({ parent });
-  const [variables] =
-    await tagManager.accounts.containers.workspaces.variables.list({ parent });
 
-  await fs.writeFile("tags.json", JSON.stringify(tags, null, 2));
-  await fs.writeFile("triggers.json", JSON.stringify(triggers, null, 2));
-  await fs.writeFile("variables.json", JSON.stringify(variables, null, 2));
+  const triggers =
+    await tagmanager.accounts.containers.workspaces.triggers.list({
+      auth: authClient,
+      parent,
+    });
+
+  const variables =
+    await tagmanager.accounts.containers.workspaces.variables.list({
+      auth: authClient,
+      parent,
+    });
+
+  await fs.writeFile("tags.json", JSON.stringify(tags.data, null, 2));
+  await fs.writeFile("triggers.json", JSON.stringify(triggers.data, null, 2));
+  await fs.writeFile("variables.json", JSON.stringify(variables.data, null, 2));
 }
 
 downloadContainer().catch(console.error);
