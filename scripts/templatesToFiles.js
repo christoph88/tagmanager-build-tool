27e7890336d4d4a30bbe8673c6e9ed260dac3b4c
christoph88 <christoph88@users.noreply.github.com>
2023-09-17 12:40:32 +0200

    only save sandboxed html to file

--- scripts/templatesToFiles.js
+++ scripts/templatesToFiles.js
@@ -10,18 +10,31 @@ workspaces.forEach((workspace) => {
   // Check if the templates directory exists in the current workspace
   const templatesDir = path.join("workspaces", workspace, "templates");
   if (fs.existsSync(templatesDir)) {
-    // Read the JSON file
-    const data = fs.readFileSync(path.join(templatesDir, "templates.json"));
-    const json = JSON.parse(data);
+    // Read all template files in the directory
+    const templateFiles = fs
+      .readdirSync(templatesDir)
+      .filter((file) => file.endsWith(".js"));
 
-    // Loop through all templates
-    json.template.forEach((template) => {
-      // Get the templateData
-      const templateData = template.templateData;
+    templateFiles.forEach((templateFile) => {
+      // Read the template file
+      const data = fs.readFileSync(
+        path.join(templatesDir, templateFile),
+        "utf8"
+      );
 
-      // Write the templateData to a new JavaScript file with the template name as the filename
-      const filename = `${template.name.replace(/ /g, "_")}.js`;
-      fs.writeFileSync(path.join(templatesDir, filename), templateData);
+      // Extract the sandboxed JavaScript code using a regular expression
+      const sandboxedJsMatch = data.match(
+        /___SANDBOXED_JS_FOR_WEB_TEMPLATE___\n\n([\s\S]*?)\n\n__/
+      );
+      if (sandboxedJsMatch?.[1]) {
+        const sandboxedJs = sandboxedJsMatch[1];
+
+        // Write the sandboxed JavaScript code to a new JavaScript file with the template name as the filename
+        const filename = `${templateFile.replace(/\.js$/, "_sandboxed.js")}`;
+        fs.writeFileSync(path.join(templatesDir, filename), sandboxedJs);
+      } else {
+        console.log(`No sandboxed JavaScript found in ${templateFile}`);
+      }
     });
   }
 });
