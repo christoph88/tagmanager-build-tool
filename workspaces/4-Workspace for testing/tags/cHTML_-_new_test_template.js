16a24da4bd81d180e4b021020be3a3a6405825bf
christoph88 <christoph88@users.noreply.github.com>
2023-09-20 10:15:45 +0200

    update test file

--- workspaces/4-Workspace for testing/tags/cHTML_-_new_test_template.js
+++ workspaces/4-Workspace for testing/tags/cHTML_-_new_test_template.js
@@ -1,10 +1,5 @@
-- console.log("this is a file I want to be diffed");
 console.log("this is a file I want to be diffed");
 console.log("this is a file I want to be diffed");
 console.log("this is a file I want to be diffed");
 console.log("this is a file I want to be diffed");
 console.log("this is a file I want to be diffed");
-
-+ 
-  console.log('fingerprint should change');
-  console.log('test upload with this new fingerprint');
