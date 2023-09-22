0aaec9dab1f867c903730793c0d2aaf0aa573343
christoph88 <christoph88@users.noreply.github.com>
2023-09-22 12:35:20 +0200

    check if array before trying to iterate

--- scripts/uploadChanges.js
+++ scripts/uploadChanges.js
@@ -29,56 +29,60 @@ async function uploadTags() {
     );
 
     // Create or update each tag
-    for (const tag of tags.tag) {
-      const tagsDir = workspaceDir + "/tags";
-      // Filter out HTML tags only
-      const htmlTag = tag.type === "html";
+    if (Array.isArray(tags.tag)) {
+      for (const tag of tags.tag) {
+        const tagsDir = workspaceDir + "/tags";
+        // Filter out HTML tags only
+        const htmlTag = tag.type === "html";
 
-      if (htmlTag) {
-        console.log(`Process Tag ${tag.name}.`);
-        // Already start reading tag file
-        const tagFile = await fs.promises.readFile(
-          `${tagsDir}/${tag.name.replace(/ /g, "_")}.html`,
-          "utf8"
-        );
+        if (htmlTag) {
+          console.log(`Process Tag ${tag.name}.`);
+          // Already start reading tag file
+          const tagFile = await fs.promises.readFile(
+            `${tagsDir}/${tag.name.replace(/ /g, "_")}.html`,
+            "utf8"
+          );
 
-        // Construct the tag object to match the Google Tag Manager API request format
-        const requestTag = {
-          path: tag.path,
-          accountId: tag.accountId,
-          containerId: tag.containerId,
-          workspaceId: tag.workspaceId,
-          tagId: tag.tagId,
-          name: tag.name,
-          parameter: tag.parameter,
-          consentSettings: tag.consentSettings,
-          monitoringMetadata: tag.monitoringMetadata,
-          priority: tag.priority,
-          type: tag.type,
-        };
+          // Construct the tag object to match the Google Tag Manager API request format
+          const requestTag = {
+            path: tag.path,
+            accountId: tag.accountId,
+            containerId: tag.containerId,
+            workspaceId: tag.workspaceId,
+            tagId: tag.tagId,
+            name: tag.name,
+            parameter: tag.parameter,
+            consentSettings: tag.consentSettings,
+            monitoringMetadata: tag.monitoringMetadata,
+            priority: tag.priority,
+            type: tag.type,
+          };
 
-        const htmlParameterIndex = requestTag.parameter?.findIndex((p) => {
-          return p.type === "template" && p.key === "html";
-        });
+          const htmlParameterIndex = requestTag.parameter?.findIndex((p) => {
+            return p.type === "template" && p.key === "html";
+          });
 
-        requestTag.parameter[htmlParameterIndex].value = tagFile;
+          requestTag.parameter[htmlParameterIndex].value = tagFile;
 
-        try {
-          const response =
-            await tagmanager.accounts.containers.workspaces.tags.update({
-              auth: authClient,
-              fingerprint: tag.fingerprint,
-              path: tag.path,
-              requestBody: requestTag,
-            });
-          console.log(`Tag ${tag.name} uploaded successfully.`);
-          console.log(response.status);
-        } catch (error) {
-          console.error(`Failed to upload tag ${tag.name}.`);
-          console.error(JSON.stringify(error.errors, null, 2));
-          console.error(error.status);
+          try {
+            const response =
+              await tagmanager.accounts.containers.workspaces.tags.update({
+                auth: authClient,
+                fingerprint: tag.fingerprint,
+                path: tag.path,
+                requestBody: requestTag,
+              });
+            console.log(`Tag ${tag.name} uploaded successfully.`);
+            console.log(response.status);
+          } catch (error) {
+            console.error(`Failed to upload tag ${tag.name}.`);
+            console.error(JSON.stringify(error.errors, null, 2));
+            console.error(error.status);
+          }
         }
       }
+    } else {
+      console.log("No templates found!");
     }
   }
 }
@@ -113,56 +117,60 @@ async function uploadVariables() {
     );
 
     // Create or update each variable
-    for (const variable of variables.variable) {
-      const variablesDir = workspaceDir + "/variables";
-      // Filter out HTML variables only
-      const jsVariable = variable.type === "jsm";
+    if (Array.isArray(variables.variable)) {
+      for (const variable of variables.variable) {
+        const variablesDir = workspaceDir + "/variables";
+        // Filter out HTML variables only
+        const jsVariable = variable.type === "jsm";
 
-      if (jsVariable) {
-        console.log(`Process Variable ${variable.name}.`);
-        // Already start reading variable file
-        const variableFile = await fs.promises.readFile(
-          `${variablesDir}/${variable.name.replace(/ /g, "_")}.js`,
-          "utf8"
-        );
+        if (jsVariable) {
+          console.log(`Process Variable ${variable.name}.`);
+          // Already start reading variable file
+          const variableFile = await fs.promises.readFile(
+            `${variablesDir}/${variable.name.replace(/ /g, "_")}.js`,
+            "utf8"
+          );
 
-        // Construct the variable object to match the Google variable Manager API request format
-        const requestVariable = {
-          path: variable.path,
-          accountId: variable.accountId,
-          containerId: variable.containerId,
-          workspaceId: variable.workspaceId,
-          variableId: variable.variableId,
-          name: variable.name,
-          parameter: variable.parameter,
-          consentSettings: variable.consentSettings,
-          monitoringMetadata: variable.monitoringMetadata,
-          priority: variable.priority,
-          type: variable.type,
-        };
+          // Construct the variable object to match the Google variable Manager API request format
+          const requestVariable = {
+            path: variable.path,
+            accountId: variable.accountId,
+            containerId: variable.containerId,
+            workspaceId: variable.workspaceId,
+            variableId: variable.variableId,
+            name: variable.name,
+            parameter: variable.parameter,
+            consentSettings: variable.consentSettings,
+            monitoringMetadata: variable.monitoringMetadata,
+            priority: variable.priority,
+            type: variable.type,
+          };
 
-        const jsParameterIndex = requestVariable.parameter?.findIndex((p) => {
-          return p.type === "template" && p.key === "javascript";
-        });
+          const jsParameterIndex = requestVariable.parameter?.findIndex((p) => {
+            return p.type === "template" && p.key === "javascript";
+          });
 
-        requestVariable.parameter[jsParameterIndex].value = variableFile;
+          requestVariable.parameter[jsParameterIndex].value = variableFile;
 
-        try {
-          const response =
-            await tagmanager.accounts.containers.workspaces.variables.update({
-              auth: authClient,
-              fingerprint: variable.fingerprint,
-              path: variable.path,
-              requestBody: requestVariable,
-            });
-          console.log(`Variable ${variable.name} uploaded successfully.`);
-          console.log(response.status);
-        } catch (error) {
-          console.error(`Failed to upload variable ${variable.name}.`);
-          console.error(JSON.stringify(error.errors, null, 2));
-          console.error(error.status);
+          try {
+            const response =
+              await tagmanager.accounts.containers.workspaces.variables.update({
+                auth: authClient,
+                fingerprint: variable.fingerprint,
+                path: variable.path,
+                requestBody: requestVariable,
+              });
+            console.log(`Variable ${variable.name} uploaded successfully.`);
+            console.log(response.status);
+          } catch (error) {
+            console.error(`Failed to upload variable ${variable.name}.`);
+            console.error(JSON.stringify(error.errors, null, 2));
+            console.error(error.status);
+          }
         }
       }
+    } else {
+      console.log("No templates found!");
     }
   }
 }
@@ -197,53 +205,58 @@ async function uploadTemplates() {
     );
 
     // Create or update each template
-    for (const template of templates.template) {
-      const templatesDir = workspaceDir + "/templates";
+    if (Array.isArray(templates.template)) {
+      for (const template of templates.template) {
+        const templatesDir = workspaceDir + "/templates";
 
-      const galleryTemplate = typeof template.galleryReference !== "undefined";
+        const galleryTemplate =
+          typeof template.galleryReference !== "undefined";
 
-      if (!galleryTemplate) {
-        console.log(`Process Template ${template.name}.`);
-        // Already start reading  file
-        const templateFile = await fs.promises.readFile(
-          `${templatesDir}/${template.name.replace(/ /g, "_")}.js`,
-          "utf8"
-        );
+        if (!galleryTemplate) {
+          console.log(`Process Template ${template.name}.`);
+          // Already start reading  file
+          const templateFile = await fs.promises.readFile(
+            `${templatesDir}/${template.name.replace(/ /g, "_")}.js`,
+            "utf8"
+          );
 
-        // Construct the template object to match the Google template Manager API request format
-        const requestTemplate = {
-          path: template.path,
-          accountId: template.accountId,
-          containerId: template.containerId,
-          workspaceId: template.workspaceId,
-          templateId: template.templateId,
-          name: template.name,
-          parameter: template.parameter,
-          consentSettings: template.consentSettings,
-          monitoringMetadata: template.monitoringMetadata,
-          priority: template.priority,
-          type: template.type,
-          templateData: template.templateData,
-        };
+          // Construct the template object to match the Google template Manager API request format
+          const requestTemplate = {
+            path: template.path,
+            accountId: template.accountId,
+            containerId: template.containerId,
+            workspaceId: template.workspaceId,
+            templateId: template.templateId,
+            name: template.name,
+            parameter: template.parameter,
+            consentSettings: template.consentSettings,
+            monitoringMetadata: template.monitoringMetadata,
+            priority: template.priority,
+            type: template.type,
+            templateData: template.templateData,
+          };
 
-        requestTemplate.templateData = templateFile;
+          requestTemplate.templateData = templateFile;
 
-        try {
-          const response =
-            await tagmanager.accounts.containers.workspaces.variables.update({
-              auth: authClient,
-              fingerprint: template.fingerprint,
-              path: template.path,
-              requestBody: requestTemplate,
-            });
-          console.log(`Template ${template.name} uploaded successfully.`);
-          console.log(response.status);
-        } catch (error) {
-          console.error(`Failed to upload template ${template.name}.`);
-          console.error(JSON.stringify(error.errors, null, 2));
-          console.error(error.status);
+          try {
+            const response =
+              await tagmanager.accounts.containers.workspaces.variables.update({
+                auth: authClient,
+                fingerprint: template.fingerprint,
+                path: template.path,
+                requestBody: requestTemplate,
+              });
+            console.log(`Template ${template.name} uploaded successfully.`);
+            console.log(response.status);
+          } catch (error) {
+            console.error(`Failed to upload template ${template.name}.`);
+            console.error(JSON.stringify(error.errors, null, 2));
+            console.error(error.status);
+          }
         }
       }
+    } else {
+      console.log("No templates found!");
     }
   }
 }
