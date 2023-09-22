c9aa52bb91864a0afbf4f5e8e02cbbc324f5893b
christoph88 <christoph88@users.noreply.github.com>
2023-09-22 20:44:22 +0200

    add todo comment search

---
+++ todo.sh
@@ -0,0 +1,5 @@
+./scripts/processEntities.js:// TODO find an easy option to transpile in vs code, do NOT do it automatically
+./scripts/processEntities.js:// TODO extract javascript from templates
+./scripts/uploadChanges.js:          // TODO fix uploading of templates, this still gives errors
+./scripts/uploadChanges.js:            // TODO remove full error
+./scripts/uploadChanges.js:  // TODO uncomment
