7027f580fb0caef084ae60c13e048f77e5404c68
christoph88 <christoph88@users.noreply.github.com>
2023-09-22 20:48:55 +0200

    colorize the output

--- todo.sh
+++ todo.sh
@@ -1,5 +1,4 @@
-./scripts/processEntities.js:// TODO find an easy option to transpile in vs code, do NOT do it automatically
-./scripts/processEntities.js:// TODO extract javascript from templates
-./scripts/uploadChanges.js:          // TODO fix uploading of templates, this still gives errors
-./scripts/uploadChanges.js:            // TODO remove full error
-./scripts/uploadChanges.js:  // TODO uncomment
+#!/bin/bash
+
+grep --color=always -r "TODO" --exclude={todo.sh} --exclude-dir={node_modules,.git} .
+
