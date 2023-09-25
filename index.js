89fbc6a486eec46dff8f4e54fc70ef9a2f4182ed
christoph88 <christoph88@users.noreply.github.com>
2023-09-25 21:46:59 +0200

    add todos, follow same flow as dbt

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
+// TODO create a local interface using commander which enabled downloading and uploading, same principle as DBT
+// TODO remove container branch locally and remote
 console.log("Hello");
