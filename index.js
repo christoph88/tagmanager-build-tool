2c1e16a5858bf125534fc02d318443fb82cf472b
christoph88 <christoph88@users.noreply.github.com>
2023-09-26 21:00:53 +0200

    install commander and create basic cli

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-// TODO create a local interface using commander which enabled downloading and uploading, same principle as DBT
-// TODO remove container branch locally and remote
-console.log("Hello");
+import { downloadContainer } from "./scripts/downloadContainer.js";
+import { uploadChanges } from "./scripts/uploadChanges.js";
+import { Command } from "commander";
+const program = new Command();
+
+program.version("0.0.1").description("Tagmanager build tool");
+
+program
+  .command("download")
+  .description("Download container")
+  .option("-p, --enableProcessing <boolean>", "Enable processing", false)
+  .action((options) => {
+    console.log(options);
+    downloadContainer(options.enableProcessing);
+  });
+
+program
+  .command("uploadChanges")
+  .description("Upload changes")
+  .option("-t, --tags <tags>", "Tags to upload", false)
+  .option("-v, --variables <variables>", "Variables to upload", false)
+  .option("-m, --template <template>", "Template to upload", true)
+  .action((options) => {
+    // TODO if options contain * upload all of them
+    // TODO add a filter to the templates which filters out the selection here
+    console.log(options);
+    uploadChanges(options.tags, options.variables, options.templates);
+  });
+
+program.parse();
